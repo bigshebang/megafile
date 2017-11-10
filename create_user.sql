39591c339dee1141e6a2571cee0345b4592b4d41
bigshebang <ljm3103@rit.edu>
2017-11-09 22:01:49 -0500

    make upload password longer, ~30 chars wasn't enough

--- create_user.sql
+++ create_user.sql
@@ -1,6 +1,6 @@
 CREATE USER 'mf-dba'@'localhost' IDENTIFIED BY 'I heard that passphrases are better than passwords 51257!$%^';
 CREATE USER 'mf-dba-del'@'localhost' IDENTIFIED BY 'when will we get rid of passwords doe? 9092211';
-CREATE USER 'mf-dba-upload'@'localhost' IDENTIFIED BY 'i like to upload file$ 12580335#!!';
+CREATE USER 'mf-dba-upload'@'localhost' IDENTIFIED BY 'i like to upload file$, is that okay? 12580335#!!';
 GRANT SELECT,INSERT on MegaFile.* to 'mf-dba'@'localhost';
 GRANT SELECT,DELETE on MegaFile.shares to 'mf-dba-del'@'localhost';
 GRANT SELECT,DELETE on MegaFile.files to 'mf-dba-del'@'localhost';
