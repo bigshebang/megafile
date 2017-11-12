37752ce558de9e82261fbf44523da1e11beebf68
bigshebang <ljm3103@rit.edu>
2017-11-11 19:31:08 -0500

    change sql user names to be dev names

--- create_user.sql
+++ create_user.sql
@@ -1,13 +1,9 @@
-CREATE USER 'mf-dba'@'localhost' IDENTIFIED BY 'mf-dba-devpass1029384756%^';
-CREATE USER 'mf-dba-del'@'localhost' IDENTIFIED BY 'mf-dba-del-devpass1029384756$%';
-CREATE USER 'mf-dba-upload'@'localhost' IDENTIFIED BY 'mf-dba-upload-devpass1029384756#$';
-CREATE USER 'mf-redis'@'localhost' IDENTIFIED BY 'mf-redis-devpass1029384756@#!';
-GRANT SELECT,INSERT on MegaFile.* to 'mf-dba'@'localhost';
-GRANT SELECT,DELETE on MegaFile.shares to 'mf-dba-del'@'localhost';
-GRANT SELECT,DELETE on MegaFile.files to 'mf-dba-del'@'localhost';
-GRANT SELECT,INSERT,UPDATE on MegaFile.files to 'mf-dba-upload'@'localhost';
-GRANT SELECT,INSERT,UPDATE on MegaFile.users to 'mf-dba-upload'@'localhost';
-GRANT SELECT on MegaFile.* to 'mf-redis'@'localhost'; #needed for reflection with sqlalchemy
-GRANT SELECT,INSERT on MegaFile.shares to 'mf-redis'@'localhost';
-GRANT SELECT,INSERT,UPDATE on MegaFile.redis_logs to 'mf-redis'@'localhost';
+CREATE USER 'mf-dba-dev'@'localhost' IDENTIFIED BY 'mf-dba-devpass1029384756%^';
+CREATE USER 'mf-dba-del-dev'@'localhost' IDENTIFIED BY 'mf-dba-del-devpass1029384756$%';
+CREATE USER 'mf-dba-upload-dev'@'localhost' IDENTIFIED BY 'mf-dba-upload-devpass1029384756#$';
+GRANT SELECT,INSERT on MegaFile.* to 'mf-dba-dev'@'localhost';
+GRANT SELECT,DELETE on MegaFile.shares to 'mf-dba-del-dev'@'localhost';
+GRANT SELECT,DELETE on MegaFile.files to 'mf-dba-del-dev'@'localhost';
+GRANT SELECT,INSERT,UPDATE on MegaFile.files to 'mf-dba-upload-dev'@'localhost';
+GRANT SELECT,INSERT,UPDATE on MegaFile.users to 'mf-dba-upload-dev'@'localhost';
 FLUSH PRIVILEGES;
