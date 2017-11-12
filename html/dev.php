aa1d152abcb5a796bfc777f09464ba7615940e66
bigshebang <ljm3103@rit.edu>
2017-11-12 17:58:45 -0500

    added all the functionality

--- html/dev.php
+++ html/dev.php
@@ -6,13 +6,124 @@ if(!isset($_SESSION['id']))
 	die;
 }
 
+
+function getElement($value, $err)
+{
+	$element = <<<TOP
+			<li class="list-group-item">
+			<div class="left" id="username-div"
+TOP;
+	if($err != "")
+	{
+		$element .= " style=\"color: red; margin-bottom: 11px;\">" . $err . "\n";
+		$element .= <<<BOTTOM
+			</div>
+			<div class="clearfix"></div>
+			</li>
+
+BOTTOM;
+	}
+	else
+	{
+		$element .= <<<BOTTOM
+			>$value
+			</div>
+			<div class="clearfix"></div>
+			</li>
+
+BOTTOM;
+	}
+
+	return $element;
+}
+
+
 require_once("functions.php");
-$conn = connect_to_db(3); #get that nice dev SQL user
+$dev_conn = connect_to_db(3); #get that nice dev SQL user
+$results = "";
+$success = "";
+$error = "";
+
+if(isset($_POST['sql']) && $_POST['sql'] != "")
+{
+	if(!strpos($_POST['sql'], 'DROP') && !strpos($_POST['sql'], 'DELETE'))
+	{
+		$result = execute_sql($dev_conn, $_POST['sql']);
+
+		$results = <<<TOP
+				<br /><br />
+				<div id="search-results">
+				<h3 class="bars">Results</h3>
+				<ul class="list-group" style="text-align: left;">
+
+TOP;
+
+			if($result[1] != "")
+			{
+				$results .= getElement("", "Error in the query.");
+			}
+			else
+			{
+				if($result[0]->num_rows < 1)
+				{
+					$results .= getElement("", "No rows returned.");
+				}
+				else
+				{
+					// $i = 0;
+					$row = $result[0]->fetch_assoc();
+					print "row: ";
+					print_r($row);
+					print "\n<br>";
+					$columns = array();
+					$rowStr = "";
+
+					#gotta get the columns!
+					foreach($row as $key => $value)
+						array_push($columns, $key);
+
+					# add each row that isn't at the end
+					for($iterator = 0; $iterator < count($columns) - 1; $iterator++)
+						$rowStr .= $columns[$iterator] . ", ";
+
+					$rowStr .= $columns[$iterator]; //add the final column with no more commas
+
+					$results .= getElement($rowStr, "");
+					//add all the resultant rows
+					do
+					{
+						$rowStr = "";
+
+						for($iterator = 0; $iterator < count($columns) - 1; $iterator++)
+							$rowStr .= $row[$columns[$iterator]] . ", ";
+
+						$rowStr .= $row[$columns[$iterator]]; //add the final column with no more commas
+
+						$results .= getElement($rowStr, "");
+					} while($row = $result[0]->fetch_assoc());
+				}
+			}
+
+			$result[0]->close(); //close mysqli_result bc we're done with it now
+
+				$results .= <<<BOTTOM
+				</ul>
+				</div>
+
+BOTTOM;
+	}
+	else
+	{
+		$sqlError = "you can't remove data; security violation. <a href=\"https://i.imgur.com/TkYVQWK.png\">BOO NOT COOL</a>.";
+	}
+}
+
+$dev_conn->close();
 ?>
 <!doctype html>
 <html>
 <head>
-<title>Mega File</title>
+<title>Share Files - Mega File</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
@@ -29,7 +140,6 @@ $conn = connect_to_db(3); #get that nice dev SQL user
 	<!-- js -->
 		 <script src="js/bootstrap.js"></script>
 	<!-- js -->
-	<!-- js -->
 		<script type="text/javascript" src="js/move-top.js"></script>
 <script type="text/javascript" src="js/easing.js"></script>
 <!--/script-->
@@ -43,15 +153,7 @@ $conn = connect_to_db(3); #get that nice dev SQL user
 </script>
 <script type="text/javascript"> 
 	function submitform(i){
-		var f = document.deleteFile;
-		if(f.tagName == "FORM")
-			f.submit();
-		else
-			f[i].submit();
-	} 
-
-	function submitform2(i){
-		var f = document.downloadFile;
+		var f = document.shareFiles;
 		if(f.tagName == "FORM")
 			f.submit();
 		else
@@ -62,42 +164,58 @@ $conn = connect_to_db(3); #get that nice dev SQL user
 	</head>
 	<body>
 		<div class="header" id="home">
-		<?php
-		// session_start();
-		require_once("menu.php");
-
-		if(isset($_SESSION['id']))
-		{
-			require_once("functions.php");
-			$conn = connect_to_db();
-			$fileID = $_SESSION['id'];
-			if(isset($_POST['accounts']))
+			<?php
+			require_once("menu.php");
+			?>
+			<div class="header-banner">
+					<!-- Top Navigation -->
+					<section class="bgi banner5"><h2>Share Files</h2> </section>
+					
+	<!-- contact -->
+	<div class="contact-top">
+		<!-- container -->
+		<div style="width: 600px; text-align: center;" class="container">
+			<div style="margin: 0 0 2.3em 0" class="mail-grids">
+				<div style="float: none; width: 100%; padding-right: 15px;" class="col-md-6 contact-form">
+					<p style="margin-bottom: 25px;">See what's in the shares table (build your own queries)</p>
+					<form action="dev.php" method="POST">
+						<input maxlength="128" type="text" name="sql" placeholder="SELECT * FROM shares;">
+						<input type="submit" value="Execute">
+					</form>
+				</div>
+				<div class="clearfix"> </div>
+			<?php
+			if($error)
 			{
-				if(is_numeric($_POST['accounts']))
-					$fileID = intval($_POST['accounts']);
-			}
+				// $safeError = htmlentities($error);
+				echo <<<ERROR
+				<div style="margin-top: 30px;">
+				<p style="color: red;">$error</p>
+				</div>
 
-			if(isset($_POST['deleteFile']))
+ERROR;
+			}
+			else if($success)
 			{
-				if($_POST['deleteFile'] == "")
-					$error = "You must give a filename to be deleted.";
-				else
-				{
-					$tempResult = deleteFile($_POST['deleteFile'], $_SESSION['id']);
-					if($tempResult)
-						$error = "There is no file with that name associated with"
-								 . " this account.";
-				}
-			}
+				echo <<<ERROR
+				<div style="margin-top: 30px;">
+				<p style="color: green;">$success</p>
+				</div>
 
-			require_once("home.php");
-			$conn->close();
-		}
-		else
-		{
-			require_once("default.php");
-		}
-		?>
+ERROR;
+			}
+			else if($results)
+			{
+				echo $results . "\n";
+			}
+			?>
+			</div>
+		</div>
+		<!-- //container -->
+	</div>
+	<!-- //contact -->
+		</div>
+</div>
 <?php
 require_once("footers.html");
 ?>
