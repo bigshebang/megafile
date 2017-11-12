3a108af1d16e25a776f8ad1a50c60ba09714ef16
bigshebang <ljm3103@rit.edu>
2017-11-11 19:31:50 -0500

    change sql db names to be dev

--- create_user.sql
+++ create_user.sql
@@ -1,9 +1,9 @@
 CREATE USER 'mf-dba-dev'@'localhost' IDENTIFIED BY 'mf-dba-devpass1029384756%^';
 CREATE USER 'mf-dba-del-dev'@'localhost' IDENTIFIED BY 'mf-dba-del-devpass1029384756$%';
 CREATE USER 'mf-dba-upload-dev'@'localhost' IDENTIFIED BY 'mf-dba-upload-devpass1029384756#$';
-GRANT SELECT,INSERT on MegaFile.* to 'mf-dba-dev'@'localhost';
-GRANT SELECT,DELETE on MegaFile.shares to 'mf-dba-del-dev'@'localhost';
-GRANT SELECT,DELETE on MegaFile.files to 'mf-dba-del-dev'@'localhost';
-GRANT SELECT,INSERT,UPDATE on MegaFile.files to 'mf-dba-upload-dev'@'localhost';
-GRANT SELECT,INSERT,UPDATE on MegaFile.users to 'mf-dba-upload-dev'@'localhost';
+GRANT SELECT,INSERT on MegaFileDev.* to 'mf-dba-dev'@'localhost';
+GRANT SELECT,DELETE on MegaFileDev.shares to 'mf-dba-del-dev'@'localhost';
+GRANT SELECT,DELETE on MegaFileDev.files to 'mf-dba-del-dev'@'localhost';
+GRANT SELECT,INSERT,UPDATE on MegaFileDev.files to 'mf-dba-upload-dev'@'localhost';
+GRANT SELECT,INSERT,UPDATE on MegaFileDev.users to 'mf-dba-upload-dev'@'localhost';
 FLUSH PRIVILEGES;
