bd39253984ec0750f8a757fb2a92ace1e6ecdc4f
bigshebang <ljm3103@rit.edu>
2017-11-11 20:43:20 -0500

    add a new dev user

--- create_user.sql
+++ create_user.sql
@@ -1,9 +1,11 @@
 CREATE USER 'mf-dba-dev'@'localhost' IDENTIFIED BY 'mf-dba-devpass1029384756%^';
 CREATE USER 'mf-dba-del-dev'@'localhost' IDENTIFIED BY 'mf-dba-del-devpass1029384756$%';
 CREATE USER 'mf-dba-upload-dev'@'localhost' IDENTIFIED BY 'mf-dba-upload-devpass1029384756#$';
+CREATE USER 'mf-dev-dev'@'localhost' IDENTIFIED BY 'mf-dev-devpassdevpass1029384756!@~';
 GRANT SELECT,INSERT on MegaFileDev.* to 'mf-dba-dev'@'localhost';
 GRANT SELECT,DELETE on MegaFileDev.shares to 'mf-dba-del-dev'@'localhost';
 GRANT SELECT,DELETE on MegaFileDev.files to 'mf-dba-del-dev'@'localhost';
 GRANT SELECT,INSERT,UPDATE on MegaFileDev.files to 'mf-dba-upload-dev'@'localhost';
 GRANT SELECT,INSERT,UPDATE on MegaFileDev.users to 'mf-dba-upload-dev'@'localhost';
+GRANT SELECT on MegaFileDev.shares to 'mf-dev-dev'@'localhost';
 FLUSH PRIVILEGES;
