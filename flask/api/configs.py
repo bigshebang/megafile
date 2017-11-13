4c71e5a8777d0548c6c4c48012cb31cfbf252519
bigshebang <ljm3103@rit.edu>
2017-11-12 19:16:37 -0500

    add "full" functionality to flask endpoints

--- flask/api/configs.py
+++ flask/api/configs.py
@@ -1,6 +1,8 @@
 #from . import api
-from flask_restplus import Resource, Namespace, fields, reqparse
+from flask_restplus import Resource, Namespace, reqparse
 import urllib
+from time import sleep
+from .utils import random_sleep_time
 
 ns = Namespace('configs', description='Fetches configs for servers, networking devices and appliances')
 
@@ -10,20 +12,23 @@ ns = Namespace('configs', description='Fetches configs for servers, networking d
 
 @ns.route('/')
 class Configs(Resource):
-	@ns.doc('fetch_document')
-	def post(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument(
-			'url',
-			type=unicode,
-			help='Must give URL to fetch config from',
-			location='form',
-		)
-		args = parser.parse_args()
+	# @ns.doc('fetch_document')
+	# @ns.doc(params={"url": "url to fetch configs from"})
+	parser = reqparse.RequestParser()
+	parser.add_argument(
+		'url',
+		type=unicode,
+		help='Must give URL to fetch config from',
+		location='form',
+	)
 
-		print "we got:",
-		print args
+	@ns.expect(parser)
+	def post(self):
+		
+		args = self.parser.parse_args()
+
+		sleep(random_sleep_time())
+
 		result = urllib.urlopen(args['url'])
 		output = result.read().decode("utf-8", errors="replace")
-		print "output: '%s'" % output
 		return output
--- flask/api/creds.py
+++ flask/api/creds.py
@@ -1,11 +1,46 @@
 #from . import api
-from flask_restplus import Resource, Namespace
+from flask_restplus import Resource, Namespace, reqparse
+from time import sleep
+from .utils import random_sleep_time, flip_coin, random_data, random_username
+from json import dumps
 
 ns = Namespace('creds', description='Fetches credentials from secret management (Vault)')
 
-#configs = ns.model('Configs'
-
 @ns.route('/')
 class Creds(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument(
+		'cred',
+		type=unicode,
+		help='Must give name of credential you want',
+		location='args',
+	)
+
+	@ns.expect(parser)
 	def get(self):
-		return 'this is the creds endpoint'
+		args = self.parser.parse_args()
+		creds = {}
+		# sleep(random_sleep_time(30, 100))
+
+		#if they give redis, reject them
+		if "redis" in args['cred'].lower():
+			return "not implemented yet"
+		#otherwise, 90% of the time return a random username:password combo
+		elif flip_coin(weight=.15):
+			#get a random username and password, put it into a dict and return json
+			username = random_username()
+			password = random_data()
+			creds = {
+				"username": username,
+				"password": password
+			}
+
+		#the other half, print out our favorite little string as the password ;)
+		#just google it
+		else:
+			creds = {
+				"username": "jon_sudano",
+				"password": "QbgGIy3kKiM"
+			}
+
+		return dumps(creds)
